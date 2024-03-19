239ce1b93f3d4ae35ca32bc01bd4a245541933c5
Nithish Reddy <75037427+nithishreddy27@users.noreply.github.com>
2024-03-20 00:31:59 +0530

    nr

---
+++ pages/checkbalance.js
@@ -0,0 +1,50 @@
+import Html5QrcodePlugin from '@/src/Html5QrcodePlugin';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+export default function Checkbalance() {
+    const [scanUser, setScanUser] = useState()
+
+    const [user, setUser] = useState()
+    const [userId, setuserId] = useState()
+
+    const onNewScanResult = (decodedText, decodedResult) => {
+        // handle decoded results here
+        console.log("decoded text ",decodedText , " ", decodedResult)
+        // handleSenderEmailChange(senderEmails.length+1, decodedText)
+        // alert(decodedResult ,decodedText)
+        // const user = decodedText;
+        setuserId(decodedText)
+        html5QrCode.stop()
+    };
+
+    const getUser = async ( ) =>{
+        const userResponse = await axios.get(`/api/getUser?userId=${userId}`)
+        if(userResponse){
+
+        }
+        setUser(userResponse.data.person)
+        // console.log("user Res ",userResponse);
+    }
+
+        useEffect(()=>{
+        if(userId){
+          getUser()
+        }
+      },[userId])
+  return (
+    <div>checkbalance
+
+        
+
+{!scanUser && (
+        <Html5QrcodePlugin
+        fps={10}
+        qrbox={250}
+        disableFlip={false}
+        qrCodeSuccessCallback={onNewScanResult}
+    />
+    )}
+    </div>
+  )
+}
